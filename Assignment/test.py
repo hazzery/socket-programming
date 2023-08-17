fa56642b035e440f1d7faa8d053de21617f91ebe
Harrison Parkes <hpa101@uclive.ac.nz>
2023-08-17 15:05:25 +1200

    Created testing script to send 256 create requests

---
+++ Assignment/test.py
@@ -0,0 +1,20 @@
+import subprocess as sp
+
+with open('names.txt') as names_file:
+    names = names_file.readlines()
+
+for name in names:
+    client_program = ["python3", "client.py", "localhost", "1024", name, "create"]
+
+    process = sp.Popen(client_program, text=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+    try:
+        output, errors = process.communicate(input="John\nHello")
+    except sp.TimeoutExpired:
+        process.kill()
+        output, errors = process.communicate()
+    print(output)
+    print(errors)
+
+    process.terminate()
+
+sp.Popen(["python3", "client.py", "localhost", "1024", "John", "read"])
