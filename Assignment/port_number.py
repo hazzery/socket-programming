bbac448d1f4dceb7be1648d06401dfc5e21c09b6
Harry Parkes <harrydparkes@proton.me>
2023-08-29 13:59:47 +1200

    Created PortNumber class to perform range check

---
+++ Assignment/port_number.py
@@ -0,0 +1,12 @@
+class PortNumber(int):
+
+    def __new__(cls, string: str):
+        try:
+            value = int(string)
+        except ValueError:
+            raise TypeError("Port number must be an integer")
+
+        if value not in range(1024, 64001):
+            raise ValueError("Port number must be in range 1024-64000")
+
+        return super(cls, cls).__new__(cls, value)
