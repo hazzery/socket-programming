5e11cdec9a6ef20b448373f6006297cd0bbac34e
Harry Parkes <harrydparkes@proton.me>
2023-09-02 13:08:46 +1200

    Added minimum and maximum port number values as class attributes

--- src/port_number.py
+++ src/port_number.py
@@ -1,12 +1,15 @@
 class PortNumber(int):
 
+    MINIMUM = 1024
+    MAXIMUM = 64000
+
     def __new__(cls, string: str):
         try:
             value = int(string)
         except ValueError:
             raise TypeError("Port number must be an integer")
 
-        if value not in range(1024, 64001):
-            raise ValueError("Port number must be in the range 1024-64000")
+        if not (cls.MINIMUM <= value <= cls.MAXIMUM):
+            raise ValueError("Port number must be in the range 1024-64000 (inclusive)")
 
         return super(cls, cls).__new__(cls, value)
