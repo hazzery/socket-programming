319ecb110c4e2c8373c87a1a9b8366787dbf9f0c
Harry Parkes <harrydparkes@proton.me>
2023-10-27 21:56:37 +1300

    Added return type hints for test files

--- tests/test_message_type.py
+++ tests/test_message_type.py
@@ -12,30 +12,30 @@ class TestMessageType(unittest.TestCase):
     Test suite for MessageType enum class.
     """
 
-    def test_read_lowercase(self):
+    def test_read_lowercase(self) -> None:
         """Test that read is parsed correctly."""
         self.assertEqual(MessageType.READ, MessageType.from_str("read"))
 
-    def test_read_uppercase(self):
+    def test_read_uppercase(self) -> None:
         """Test that READ is parsed correctly."""
         self.assertEqual(MessageType.READ, MessageType.from_str("READ"))
 
-    def test_create_lowercase(self):
+    def test_create_lowercase(self) -> None:
         """Test that create is parsed correctly."""
         self.assertEqual(MessageType.CREATE, MessageType.from_str("create"))
 
-    def test_create_uppercase(self):
+    def test_create_uppercase(self) -> None:
         """Test that CREATE is parsed correctly."""
         self.assertEqual(MessageType.CREATE, MessageType.from_str("CREATE"))
 
-    def test_response_lowercase(self):
+    def test_response_lowercase(self) -> None:
         """Test that response is parsed correctly."""
         self.assertEqual(MessageType.RESPONSE, MessageType.from_str("response"))
 
-    def test_response_uppercase(self):
+    def test_response_uppercase(self) -> None:
         """Test that RESPONSE is parsed correctly."""
         self.assertEqual(MessageType.RESPONSE, MessageType.from_str("RESPONSE"))
 
-    def test_invalid(self):
+    def test_invalid(self) -> None:
         """Test that invalid input raises a ValueError."""
         self.assertRaises(ValueError, MessageType.from_str, "invalid")
--- tests/test_port_number.py
+++ tests/test_port_number.py
@@ -12,22 +12,22 @@ class TestPortNumber(unittest.TestCase):
     Test suite for PortNumber class.
     """
 
-    def test_non_numeric(self):
+    def test_non_numeric(self) -> None:
         """Test that non-numeric input raises a TypeError."""
         self.assertRaises(TypeError, PortNumber, 'abc')
 
-    def test_below_bound(self):
+    def test_below_bound(self) -> None:
         """Test that input below the lower bound raises a ValueError."""
         self.assertRaises(ValueError, PortNumber, str(PortNumber.MINIMUM - 1))
 
-    def test_lower_bound(self):
+    def test_lower_bound(self) -> None:
         """Test that input at the lower bound is accepted."""
         self.assertEqual(PortNumber.MINIMUM, PortNumber(str(PortNumber.MINIMUM)))
 
-    def test_upper_bound(self):
+    def test_upper_bound(self) -> None:
         """Test that input at the upper bound is accepted."""
         self.assertEqual(PortNumber.MAXIMUM, PortNumber(str(PortNumber.MAXIMUM)))
 
-    def test_above_bound(self):
+    def test_above_bound(self) -> None:
         """Test that input above the upper bound raises a ValueError."""
         self.assertRaises(ValueError, PortNumber, str(PortNumber.MAXIMUM + 1))
