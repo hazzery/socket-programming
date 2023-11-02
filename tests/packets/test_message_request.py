8df905c705d8bc29d3445fc01f517efd00c3fb26
Harry Parkes <harrydparkes@proton.me>
2023-11-02 20:58:58 +1300

    Added message request decoding test suite

--- tests/packets/test_message_request.py
+++ tests/packets/test_message_request.py
@@ -8,7 +8,7 @@ from src.message_type import MessageType
 from src.packets.message_request import MessageRequest
 
 
-class TestMessageRequest(unittest.TestCase):
+class TestMessageRequestEncoding(unittest.TestCase):
     """
     Test suite for MessageRequest packets
     """
@@ -130,3 +130,106 @@ class TestMessageRequest(unittest.TestCase):
         expected = message
         actual = packet[start_index : start_index + len(message.encode())].decode()
         self.assertEqual(expected, actual)
+
+
+class TestMessageRequestDecoding(unittest.TestCase):
+    """
+    Test suite for MessageRequest packets
+    """
+
+    def setUp(self) -> None:
+        """Sets up the test case"""
+        self.message_type = MessageType.CREATE
+        self.user_name = "Jamie"
+        self.receiver_name = "Jonty"
+        self.message = "Hello, World!"
+        self.packet = MessageRequest(
+            self.message_type, self.user_name, self.receiver_name, self.message
+        ).to_bytes()
+
+    def test_message_type_decoding(self) -> None:
+        """Tests that the message type is decoded correctly"""
+        decoded_packet = MessageRequest.decode_packet(self.packet)
+        expected = self.message_type
+        actual = decoded_packet[0]
+        self.assertEqual(expected, actual)
+
+    def test_user_name_decoding(self) -> None:
+        """Tests that the user's name is decoded correctly"""
+        decoded_packet = MessageRequest.decode_packet(self.packet)
+        expected = self.user_name
+        actual = decoded_packet[1]
+        self.assertEqual(expected, actual)
+
+    def test_receiver_name_decoding(self) -> None:
+        """Tests that the receiver's name is decoded correctly"""
+        decoded_packet = MessageRequest.decode_packet(self.packet)
+        expected = self.receiver_name
+        actual = decoded_packet[2]
+        self.assertEqual(expected, actual)
+
+    def test_message_decoding(self) -> None:
+        """Tests that the message is decoded correctly"""
+        decoded_packet = MessageRequest.decode_packet(self.packet)
+        expected = self.message
+        actual = decoded_packet[3].decode()
+        self.assertEqual(expected, actual)
+
+    def test_incorrect_magic_number(self) -> None:
+        """Tests that an exception is raised if the magic number is incorrect"""
+        self.packet = bytearray(self.packet)
+        self.packet[0] = 0
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
+
+    def test_invalid_message_type(self) -> None:
+        """Tests that an exception is raised if the message type is invalid"""
+        self.packet = bytearray(self.packet)
+        self.packet[2] = 0
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
+
+    def test_insufficient_user_name_length(self) -> None:
+        """Tests that an exception is raised if the length of the user's name is zero"""
+        self.packet = bytearray(self.packet)
+        self.packet[3] = 0
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
+
+    def test_non_zero_receiver_name_length_for_read(self) -> None:
+        """Tests that an exception is raised if the length of
+        the receiver's name is non-zero for a read request"""
+        self.packet = bytearray(self.packet)
+        self.packet[2] = MessageType.READ.value
+        self.packet[4] = 1
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
+
+    def test_non_zero_message_length_for_read(self) -> None:
+        """Tests that an exception is raised if the length of
+        the message is non-zero for a read request"""
+        self.packet = bytearray(self.packet)
+        self.packet[2] = MessageType.READ.value
+        self.packet[4] = 0
+        self.packet[6] = 1
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
+
+    def test_insufficient_receiver_name_length_for_create(self) -> None:
+        """Tests that an exception is raised if the length of
+        the receiver's name is zero for a create request"""
+        self.packet = bytearray(self.packet)
+        self.packet[2] = MessageType.CREATE.value
+        self.packet[4] = 0
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
+
+    def test_insufficient_message_length_for_create(self) -> None:
+        """Tests that an exception is raised if the length of
+        the message is zero for a create request"""
+        self.packet = bytearray(self.packet)
+        self.packet[2] = MessageType.CREATE.value
+        self.packet[5] = 0
+        self.packet[6] = 0
+
+        self.assertRaises(ValueError, MessageRequest.decode_packet, self.packet)
