62c7393f90e1ce82fd7524f518a8cd8e5525f9e5
Harry Parkes <harrydparkes@proton.me>
2023-11-02 21:27:46 +1300

    Created message response decoding test suite

--- tests/packets/test_message_response.py
+++ tests/packets/test_message_response.py
@@ -8,9 +8,9 @@ from src.message_type import MessageType
 from src.packets.message_response import MessageResponse
 
 
-class TestMessageResponse(unittest.TestCase):
+class TestMessageResponseEncoding(unittest.TestCase):
     """
-    Test suite for MessageResponse packets
+    Test suite for encoding MessageResponse packets
     """
 
     def test_magic_number_encoding(self) -> None:
@@ -51,3 +51,75 @@ class TestMessageResponse(unittest.TestCase):
         expected = False
         actual = packet[4]
         self.assertEqual(expected, actual)
+
+
+class TestMessageResponseDecoding(unittest.TestCase):
+    """
+    Test suite for decoding MessageResponse packets
+    """
+
+    def test_messages_decoding(self) -> None:
+        """Tests that the messages are decoded correctly"""
+        messages = [
+            ("Harry", "Hello John!".encode()),
+            ("John", "Hello Harry!".encode()),
+        ]
+        packet = MessageResponse(messages).to_bytes()
+
+        expected = [
+            ("Harry", "Hello John!"),
+            ("John", "Hello Harry!"),
+        ]
+        actual = MessageResponse.decode_packet(packet)[0]
+        self.assertEqual(expected, actual)
+
+    def test_more_messages_decoding_false(self) -> None:
+        """Tests that the more messages flag is decoded correctly"""
+        messages = [
+            ("Harry", "Hello John!".encode()),
+            ("John", "Hello Harry!".encode()),
+        ]
+        packet = MessageResponse(messages).to_bytes()
+
+        expected = False
+        actual = MessageResponse.decode_packet(packet)[1]
+        self.assertEqual(expected, actual)
+
+    def test_more_messages_decoding_true(self) -> None:
+        """Tests that the more messages flag is decoded correctly"""
+        messages = [("Harry", "Hello John!".encode())] * 256
+        packet = MessageResponse(messages).to_bytes()
+
+        expected = True
+        actual = MessageResponse.decode_packet(packet)[1]
+        self.assertEqual(expected, actual)
+
+    def test_incorrect_magic_number(self) -> None:
+        """Tests that a ValueError is raised when the magic number is incorrect"""
+        messages: list[tuple[str, bytes]] = []
+        packet = MessageResponse(messages).to_bytes()
+
+        packet = bytearray(packet)
+        packet[0] = 0x00
+
+        self.assertRaises(ValueError, MessageResponse.decode_packet, bytes(packet))
+
+    def test_invalid_message_type(self) -> None:
+        """Tests that a ValueError is raised when the message type is invalid"""
+        messages: list[tuple[str, bytes]] = []
+        packet = MessageResponse(messages).to_bytes()
+
+        packet = bytearray(packet)
+        packet[2] = 4
+
+        self.assertRaises(ValueError, MessageResponse.decode_packet, bytes(packet))
+
+    def test_incorrect_message_type(self) -> None:
+        """Tests that a ValueError is raised when the message type is incorrect"""
+        messages: list[tuple[str, bytes]] = []
+        packet = MessageResponse(messages).to_bytes()
+
+        packet = bytearray(packet)
+        packet[2] = MessageType.CREATE.value
+
+        self.assertRaises(ValueError, MessageResponse.decode_packet, bytes(packet))
