0080050f152b128d81d6213c45568d2f9006bd5c
Harry Parkes <harrydparkes@proton.me>
2023-12-11 18:48:28 +1300

    module docstrings in server/client __init__.py

--- client/__init__.py
+++ client/__init__.py
@@ -1,1 +1,13 @@
-from .client import Client as Client
+"""
+Client package
+==================
+
+The client package is directly executable using::
+
+    python3 -m client <hostname> <port_number> <username> <message_type>
+
+"""
+
+from .client import Client
+
+__all__ = ["Client"]
--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,13 @@
-from .server import Server as Server
+"""
+Server package
+==================
+
+The server package is directly executable using::
+
+    python3 -m server <port_number>
+
+"""
+
+from .server import Server
+
+__all__ = ["Server"]
