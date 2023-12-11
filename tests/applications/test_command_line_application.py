bede04f30e438bc83ea787efc76f4d768d3f7e91
Harry Parkes <harrydparkes@proton.me>
2023-12-11 21:19:53 +1300

    added test to ensure Packet cannot be subclassed without a struct format specified

---
+++ tests/applications/test_command_line_application.py
@@ -0,0 +1,35 @@
+"""
+Packet class test suite
+"""
+import unittest
+from typing import Any
+
+from src.packets.packet import Packet
+
+
+class TestClientParseArguments(unittest.TestCase):
+    """
+    Test suite for Client class
+    """
+
+    def test_fail_subclass(self) -> None:
+        """
+        Test that we cannot subclass from CommandLineApplication without struct format
+        """
+        try:
+
+            class NoStructFormat(Packet):
+                def __init__(self, *args: tuple[Any, ...]):
+                    pass
+
+                def to_bytes(self) -> bytes:
+                    return bytes()
+
+                @classmethod
+                def decode_packet(cls, packet: bytes) -> tuple[Any, ...]:
+                    return ()
+
+        except ValueError:
+            pass
+        else:
+            self.fail("Did not raise, was able to subclass from CommandLineApplication")
