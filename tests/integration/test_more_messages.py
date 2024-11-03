54bb66e627adfaa3e94c8b35bc7fe8def3289bae
Harry Parkes <harrydparkes@proton.me>
2024-11-03 17:29:24 +1300

    created new integtration test to verify more messages functionality

---
+++ tests/integration/test_more_messages.py
@@ -0,0 +1,55 @@
+"""More messages functionality test suite."""
+
+import pathlib
+import sys
+import threading
+import unittest
+
+sys.path.insert(0, "../../")
+import client
+import server
+from src.packets.message_response import MessageResponse
+
+
+class TestMoreMessages(unittest.TestCase):
+    """Test suite for the more_messages functionality.
+
+    When a client makes a read request to the server and has more than
+    255 unread messages, the server should only send the first 255. The
+    server should set the ``more_messages`` flag in the read response so
+    that the client is informed they did not receive all of the
+    available messages.
+    """
+
+    HOST_NAME = "localhost"
+    PORT_NUMBER = "1024"
+    USER_NAME = "John"
+
+    def test_more_messages(self) -> None:
+        """Tests that no more than 255 messages are sent in a read request."""
+        names = pathlib.Path("tests/resources/names.txt").read_text().splitlines()
+
+        server_object = server.Server([self.PORT_NUMBER])
+        server_thread = threading.Thread(target=server_object.run)
+        server_thread.start()
+
+        for name in names:
+            client.Client(
+                [self.HOST_NAME, self.PORT_NUMBER, name, "create"],
+            ).run(receiver_name=self.USER_NAME, message="Hello")
+
+        final_client = client.Client(
+            [self.HOST_NAME, self.PORT_NUMBER, self.USER_NAME, "read"],
+        )
+        final_client.run()
+
+        server_object.stop()
+        server_thread.join()
+
+        messages, more_messages = MessageResponse.decode_packet(final_client.result)
+        self.assertEqual(255, len(messages))
+        self.assertTrue(more_messages)
+
+
+if __name__ == "__main__":
+    unittest.main()
